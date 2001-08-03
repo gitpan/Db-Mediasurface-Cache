3b490f5bca26ad26920adaa76e7b1c9397f8672b
Nigel Wetters Gourlay <nwetters@cpan.org>
2001-08-03 12:42:11 +0000

    Import of NWETTERS/Db-Mediasurface-Cache-0.04 from CPAN.
    gitpan-cpan-distribution: Db-Mediasurface-Cache
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         NWETTERS/Db-Mediasurface-Cache-0.04.tar.gz
    gitpan-cpan-author:       NWETTERS
    gitpan-cpan-maturity:     released

--- Cache.pm
+++ Cache.pm
@@ -1,92 +1,65 @@
 package Db::Mediasurface::Cache;
-$VERSION = 0.03;
+$VERSION = 0.04;
 use strict;
 use Carp;
-use constant NEXT  => 0;
-use constant KEY   => 1;
-use constant VALUE => 2;
 
-sub new
-{
+use constant VALUE => 0;
+use constant COUNT => 1;
+
+sub new {
     my ($class, %arg) = @_;
-    my $list;
-    if (defined $arg{size}){
-	$list = [ undef, undef, undef ];
-	$list->[KEY] = $list; # special case -- points to end of list
-    }
-    bless {
-	_max_size  => $arg{size} || undef,
-	_curr_size => 0,
-	_list     => $list
-	}, $class;
+    my $self = { hits => 0,
+		 size => 0,
+		 maxsize => $arg{size},
+		 hash => undef
+		 };
+    bless $self, $class;
 }
 
-sub get
-{
-    my ($self,$key) = @_;
+sub get {
+    my ($self, $key) = @_;
     return unless defined $key;
-    my $value = undef;
-    if (defined $self->{_max_size}){
-	for (my $prev = $self->{_list}; my $elem = $prev->[NEXT]; $prev = $elem){
-	    if ($elem->[KEY] eq $key){
-		$value = $elem->[VALUE];
-		if (defined $elem->[NEXT]){
-		    $prev->[NEXT] = $elem->[NEXT];
-		    $self->{_list}->[KEY] = $self->{_list}->[KEY]->[NEXT] = [ undef, $key, $value ];
-		}
-		last;
-	    }
-	}
-    } else {
-	if (exists $self->{_list}->{$key}){
-	    $value = $self->{_list}->{$key};
-	}
+    return unless exists $self->{hash}->{$key};
+    my $value = $self->{hash}->{$key}->[VALUE];
+    if (defined $self->{maxsize}){
+	$self->{hash}->{$key}->[COUNT]++;
+	$self->{hits} ++;
     }
     return $value;
 }
 
-sub set
-{
-    my ($self,%hash) = @_;
-    if (defined $self->{_max_size}){
-	foreach my $key (keys %hash){
-	    $self->unset($key);
-	    $self->{_list}->[KEY] = $self->{_list}->[KEY]->[NEXT] = [ undef, $key, $hash{$key} ];
-	    $self->{_curr_size} ++;
-	}
-	while ( $self->{_curr_size} > $self->{_max_size} ){
-	    $self->{_list}->[NEXT] = $self->{_list}->[NEXT]->[NEXT];
-	    $self->{_curr_size} --;
-	}
+sub set {
+    my ($self, $key, $value) = @_;
+    return unless defined $key;
+    if (exists $self->{hash}->{$key}){
+	$self->{hash}->{$key}->[VALUE] = $value;
+	$self->{hits} = $self->{hits} - $self->{hash}->{$key}->[COUNT];
+	$self->{hash}->{$key}->[COUNT] = 0;
     } else {
-	foreach my $key (keys %hash){
-	    $self->{_list}->{$key} = $hash{$key};
+	$self->{hash}->{$key}->[VALUE] = $value;
+	$self->{hash}->{$key}->[COUNT] = 0;
+	$self->{size} ++;
+    }
+    if ((defined $self->{maxsize}) && ($self->{size} >= $self->{maxsize})){
+	my $cutoff = int($self->{hits} / $self->{size}) + 1;
+	foreach my $test_key (keys %{$self->{hash}}){
+	    if ( $self->{hash}->{$test_key}->[COUNT] > $cutoff ){
+		$self->{hash}->{$test_key}->[COUNT] = 0;
+	    } else {
+		delete $self->{hash}->{$test_key};
+		$self->{size} --;
+	    }
 	}
+	$self->{hits} = 0;
     }
 }
 
-sub unset
-{
+sub unset {
     my ($self,$key) = @_;
     return unless defined $key;
-    if (defined $self->{_max_size}){
-	for (my $prev = $self->{_list}; my $elem = $prev->[NEXT]; $prev = $elem){
-	    if ($elem->[KEY] eq $key){
-		$self->{_curr_size} --;
-		if (defined $elem->[NEXT]){
-		    $prev->[NEXT] = $elem->[NEXT];
-		} else {
-		    $prev->[NEXT] = undef;
-		    $self->{_list}->[KEY] = $prev;
-		}
-		last;
-	    }
-	}
-    } else {
-	if (exists $self->{_list}->{$key}){
-	    delete $self->{_list}->{$key};
-	}
-    }
+    return unless exists $self->{hash}->{$key};
+    $self->{hits} = $self->{hits} - $self->{hash}->{$key}->[COUNT];
+    delete $self->{hash}->{$key};
 }
 
 1;
@@ -97,7 +70,7 @@ Db::Mediasurface::Cache - caches a specified number of key-value pairs, disgardi
 
 =head1 VERSION
 
-This document refers to version 0.03 of DB::Mediasurface::Cache, released July 24, 2001.
+This document refers to version 0.04 of DB::Mediasurface::Cache, released August 3, 2001.
 
 =head1 SYNOPSIS
 
@@ -119,7 +92,7 @@ unless (defined ($id = $cache->get($url)))
 
 =head2 Overview
 
-Mediasurface relies on retrieving a unique ID for almost every object lookup. This module aims to cache url->id lookups in memory. The module works with a linked list, which is significantly slower at lookups than a standard Perl hash. However, the linked list allows commonly used key-value pairs to be stored towards the 'fresh' end of the list, and seldomly used pairs to drift towards the 'stale' end, from where they will eventually be pushed into oblivion, should the cache reach its maximum size. Basically, it's a trade-off between size and speed - the module will perform best when you need to perform lots of lookups of a wide range of urls, but the majority of lookups are contained within a much smaller subset of urls. Be warned - this module will cause a *reduction* in performance if used for single lookups, or if lookups are non-repetitive.
+Mediasurface relies on retrieving a unique ID for almost every object lookup. This module aims to cache url->id lookups in memory. The module allows commonly used key-value pairs to be stored towards the 'fresh' end of the store, and seldomly used pairs to drift towards the 'stale' end, from where they will eventually be pushed into oblivion, should the cache reach its maximum size. Basically, it's a trade-off between size and speed - the module will perform best when you need to perform lots of lookups of a wide range of urls, but the majority of lookups are contained within a much smaller subset of urls.
 
 =head2 Constructor
 
@@ -127,7 +100,7 @@ Mediasurface relies on retrieving a unique ID for almost every object lookup. Th
 
 =item $cache = Db::Mediasurface::Cache->new(size=>1000);
 
-This class method constructs a new cache. the size parameter can be used to set the maximum number of key-value pairs to be cached. If size is omitted, the cache defaults to using a straight hash, which is a *lot* faster than the linked list method, but which has no protection from eating all your available RAM [NOTE: this is a change in behaviour from version 0.02].
+This class method constructs a new cache. the size parameter can be used to set the maximum number of key-value pairs to be cached. If size is omitted, the cache defaults to using an infinite store, which has no protection from eating all your available RAM [NOTE: this is a change in behaviour from version 0.02].
 
 =back
 
@@ -135,13 +108,11 @@ This class method constructs a new cache. the size parameter can be used to set 
 
 =over 4
 
-=item $cache->set($key_1,$value_1...$key_n,$value_n)
+=item $cache->set($key,$value)
 
-=item $cache->set(%key_value_hash)
+Sets key-value pairs. [Note that this method can now only accept *one* key-value pair. This is a change of behaviour from version 0.03].
 
-Sets key-value pairs.
-
-=item $id = $cache->get($key1);
+=item $id = $cache->get($key);
 
 Gets the value of a given key. Returns the value, or undef if the key doesn't exist.
 
